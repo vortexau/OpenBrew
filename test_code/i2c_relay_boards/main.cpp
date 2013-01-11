d06a2d46f9aa3506ed2359066d4abedfa73c0192
vortexau <james.mclean@gmail.com>
2013-01-11 14:19:49 -0800

    Update test_code/i2c_relay_boards/main.cpp
    Added some code to roll through the relays one by one, as a test. Not yet tested.

--- test_code/i2c_relay_boards/main.cpp
+++ test_code/i2c_relay_boards/main.cpp
@@ -6,7 +6,14 @@
 
 using namespace std;
 
-#define relay_board 0x38;
+#define RELAY_BOARD 0x20; //30a Relay Board
+#define RELAY_ON    1;
+#define RELAY_OFF   0;	
+
+int pin_array[2][8] = {
+  0B01111111, 0B10111111, 0B11011111, 0B11101111, 0B11110111, 0B11111011, 0B11111101, 0B11111110,
+  0B10000000, 0B01000000, 0B00100000, 0B00010000, 0B00001000, 0B00000100, 0B00000010, 0B00000001
+};
 
 int main() {
 
@@ -21,10 +28,23 @@ int main() {
 		return (1);
 	}
 
-	if (ioctl(file, I2C_SLAVE, 0x20) < 0) {
-		cout << "I2C_SLAVE address " << 0x20 << " failed..." << endl;
+	if (ioctl(file, I2C_SLAVE, RELAY_BOARD) < 0) {
+		cout << "I2C_SLAVE address " << RELAY_BOARD << " failed..." << endl;
 		return (2);
 	}
+
+    // scroll thtrough the relays, switch on, then off.
+    for(int i = 0; i < 8; i++) {
+        
+        relayAction(file, RELAY_BOARD, i, RELAY_ON);
+        
+        sleep(1);
+        
+        relayAction(file, RELAY_BOARD, i, RELAY_OFF);
+        
+        sleep(1)
+        
+    }
 
 	// Turn all the relays on. We do this by sending 0
 	if (i2c_smbus_write_byte(file, 0x00) < 0) {
@@ -52,5 +72,21 @@ int main() {
 	close(file);
 
 	return 0;
+}
+
+void relayAction(int file, int addr, int pin, int x) {
+  //state = PCF8574.read(addr);   
+  // Read the current state fo the relay board.
+  state = i2c_smbus_read_byte(file);
+
+  // Using bitwise, add or subtract the relay you wish to turn on or off.
+  if (x == RELAY_ON) { 
+      state &= pin_array[0][pin];                      
+  } else if (x == RELAY_OFF) {
+      state |= pin_array[1][pin]; 
+  }
+  
+  //PCF8574.write(addr, state);       // ON
+  i2c_smbus_write_bite(file, state);
 }
 
