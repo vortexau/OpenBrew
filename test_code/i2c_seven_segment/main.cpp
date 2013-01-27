9db645bba4d09f51a320c43303f4a2613962ca68
James McLean <james.mclean@gmail.com>
2013-01-28 09:42:59 +1030

    refactored code to try to get it to display correctly

--- test_code/i2c_seven_segment/main.cpp
+++ test_code/i2c_seven_segment/main.cpp
@@ -36,10 +36,26 @@ static const uint8_t numbertable[] = {
 };
 
 uint16_t displaybuffer[8];
+int file;
+
+int beginTransmission();
+void endTransmission();
+void startOscillator();
+void displayOnBlinkOff();
+void setBrightness();
+void sendCharacters();
+
 
 int main() {
 
-	int file;
+	startOscillator();
+	displayOnBlinkOff();
+	setBrightness();
+	sendCharacters();
+
+}
+
+int beginTransmission() {
 
 	// BeagleBone specific I2C bus
 	if ((file = open("/dev/i2c-3", O_RDWR)) < 0) {
@@ -52,66 +68,46 @@ int main() {
 		return (2);
 	}
 
-	//state = PCF8574.read(addr);
-	// Read the current state fo the relay board.
-	//int state = i2c_smbus_read_byte(file);
-
-	// Build a string to send to the 7segment display.
-	// Awesome overview on how to build characters for the 7seg display here:
-	// http://forums.adafruit.com/viewtopic.php?f=47&t=29897#p156582
-
-	// Process of writing to the display.
-
-	// Then to use his example:
-
-//	// send 0x71 // = F
-//	displaybuffer[0] = 0x71;
-//	// send 0x3E // = U
-//	displaybuffer[1] = 0x3E;
-//	// send 0x00 // = colon off
-//	displaybuffer[2] = 0x00;
-//	// send 0x79 // = E
-//	displaybuffer[3] = 0x79;
-//	// send 0x38 // = L
-//	displaybuffer[4] = 0x38;
-
-	// send 0x71 // = F
-	displaybuffer[0] = 0x3F; // 0
-	// send 0x3E // = U
-	displaybuffer[1] = 0x06; // 1
-	// send 0x00 // = colon off
-	displaybuffer[2] = 0x00;
-	// send 0x79 // = E
-	displaybuffer[3] = 0x5B; // 2
-	// send 0x38 // = L
-	displaybuffer[4] = 0x4F; // 3
-	
-	// send 0x21 - start the oscillator
-	i2c_smbus_write_byte(file, 0x21);
-	
-	// send 0x81 - turn blink off and display on
-//	i2c_smbus_write_byte(file, 0x81);
-	// Hex is BLINK_CMD, DISPLAY_ON, BLINK_OFF
-	i2c_smbus_write_byte(file, 0x80 | 0x01 | (0 << 1));
-	
-	// send 0xEF - set brightness to max (not really required, but a bit of a sanity check).
-	i2c_smbus_write_byte(file, 0xE0 | 15);
-
-
-	i2c_smbus_write_byte(file, 0x00); // Starting RAM address. Required.
-	
-
-
-	// loop through the displaybuffer and write the bytes.
-//	for (uint8_t i=0; i<8; i++) {
-//    		//Wire.write(displaybuffer[i] & 0xFF);
-//    		i2c_smbus_write_byte(file, displaybuffer[i]);
-// 	}
-
-
-
-//	i2c_smbus_write_byte(file, state);
-
-	close(file);
-
+	return 1;
 }
+
+void endTransmission() {
+	close(file);
+}
+
+void startOscillator() {
+	beginTransmission();
+	i2c_smbus_write_byte(file, 0x21);
+	endTransmission();
+}
+
+void displayOnBlinkOff() {
+	beginTransmission();
+	i2c_smbus_write_byte(file, 0x80 | 0x01 | (0 << 1));
+	endTransmission();
+}
+
+void setBrightness() {
+	beginTransmission();
+	i2c_smbus_write_byte(file, 0xE0 | 15);
+	endTransmission();
+}
+
+void sendCharacters() {
+
+	beginTransmission();
+
+	displaybuffer[0] = 0x3F; // 0
+	displaybuffer[1] = 0x06; // 1
+	displaybuffer[2] = 0x00; // colon off
+	displaybuffer[3] = 0x5B; // 2
+	displaybuffer[4] = 0x4F; // 3
+
+	for (uint8_t i=0; i<8; i++) {
+		//Wire.write(displaybuffer[i] & 0xFF);
+		i2c_smbus_write_byte(file, displaybuffer[i] & 0xFF);
+	}
+
+	endTransmission();
+}
+
