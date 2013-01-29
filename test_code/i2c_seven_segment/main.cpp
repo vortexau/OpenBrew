ac46723985e06e6fac2623bf482ee841f7ed2eaf
James McLean <james.mclean@gmail.com>
2013-01-29 21:46:49 +1030

    working on loading spinners for each digit

--- test_code/i2c_seven_segment/main.cpp
+++ test_code/i2c_seven_segment/main.cpp
@@ -124,6 +124,8 @@ void sendCharacters() {
 
 	for(;;) {
 
+		// length 8 works best so far.
+
 		displaybuffer[0] = 0x01;
 		displaybuffer[1] = 0x01;
 		displaybuffer[2] = 0x00;
@@ -131,7 +133,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x01;
 		i2c_smbus_write_i2c_block_data(file, 0x00, 7, (__u8 *)displaybuffer);
 
-		usleep(350);
+		usleep(500);
 
 		displaybuffer[0] = 0x02;
 		displaybuffer[1] = 0x02;
@@ -140,7 +142,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x02;
 		i2c_smbus_write_i2c_block_data(file, 0x00, 7, (__u8 *)displaybuffer);
 
-		usleep(350);
+		usleep(500);
 
 		displaybuffer[0] = 0x04;
 		displaybuffer[1] = 0x04;
@@ -149,7 +151,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x04;
 		i2c_smbus_write_i2c_block_data(file, 0x00, 7, (__u8 *)displaybuffer);
 
-		usleep(350);
+		usleep(500);
 
 		displaybuffer[0] = 0x08;
 		displaybuffer[1] = 0x08;
@@ -158,7 +160,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x08;
 		i2c_smbus_write_i2c_block_data(file, 0x00, 7, (__u8 *)displaybuffer);
 
-		usleep(350);
+		usleep(500);
 
 		displaybuffer[0] = 0x10;
 		displaybuffer[1] = 0x10;
@@ -167,7 +169,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x10;
 		i2c_smbus_write_i2c_block_data(file, 0x00, 7, (__u8 *)displaybuffer);
 
-		usleep(350);
+		usleep(500);
 
 		displaybuffer[0] = 0x20;
 		displaybuffer[1] = 0x20;
@@ -176,7 +178,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x20;
 		i2c_smbus_write_i2c_block_data(file, 0x00, 7, (__u8 *)displaybuffer);
 
-		usleep(300);
+		usleep(500);
 
 	}
 
