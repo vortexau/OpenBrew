2295570a3cc8ac3a4e76521080f28f547665a545
James McLean <james.mclean@gmail.com>
2013-02-01 22:01:24 +1030

    trying a normal write with write

--- test_code/i2c_seven_segment/main.cpp
+++ test_code/i2c_seven_segment/main.cpp
@@ -73,7 +73,7 @@ void sendCharacters() {
 	i2c_smbus_write_byte(file, 0xE0 | 15); // set brightness to max
 	i2c_smbus_write_byte(file, 0x81); // display on, blink off
 
-	i2c_smbus_write_byte(file, 0x00); // starting RAM Address
+//	i2c_smbus_write_byte(file, 0x00); // starting RAM Address
 
 	/*
 	cout << "First numbers " << endl;
@@ -127,10 +127,10 @@ void sendCharacters() {
 		// length 8 works best so far.
 
 
-		displaybuffer[0] = 0x01;
+		displaybuffer[0] = 0x00;
 		displaybuffer[1] = 0x01;
-		displaybuffer[2] = 0x01; // colon.
-		displaybuffer[3] = 0x01;
+		displaybuffer[2] = 0x01;
+		displaybuffer[3] = 0x01; // colon.
 		displaybuffer[4] = 0x01;
 		displaybuffer[5] = 0x01;
 		displaybuffer[6] = 0x01;
