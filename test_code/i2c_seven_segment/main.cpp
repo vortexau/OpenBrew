859b4caa11d484465d9b65ce87e3d7ef9ce23664
James McLean <james.mclean@gmail.com>
2013-02-01 21:54:55 +1030

    trying a normal write with write

--- test_code/i2c_seven_segment/main.cpp
+++ test_code/i2c_seven_segment/main.cpp
@@ -142,10 +142,19 @@ void sendCharacters() {
 
 		// i2c_smbus_write_i2c_block_data(file, 0x00, 8, (__u8 *)displaybuffer);
 
-		usleep(400);
+		displaybuffer[0] = 0x00;
+		displaybuffer[1] = 0x00;
+		displaybuffer[2] = 0x00; // colon.
+		displaybuffer[3] = 0x00;
+		displaybuffer[4] = 0x00;
+		displaybuffer[5] = 0x00;
+		displaybuffer[6] = 0x00;
+		displaybuffer[7] = 0x00;
+
+		i2c_smbus_write_i2c_block_data(file, 0x00, 8, (__u8 *)displaybuffer);
 
 		cout << "loop" << endl;
-
+		sleep(1);
 
 		/*
 
