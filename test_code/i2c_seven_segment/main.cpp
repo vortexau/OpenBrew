8274850dbdaea3939115d5e913479497ed5fac3c
James McLean <james.mclean@gmail.com>
2013-03-30 10:05:46 +1030

    shorten timer

--- test_code/i2c_seven_segment/main.cpp
+++ test_code/i2c_seven_segment/main.cpp
@@ -131,7 +131,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x01;
 		i2c_smbus_write_i2c_block_data(file, 0x00, sizeof(displaybuffer), (__u8 *)displaybuffer);
 
-		usleep(5000);
+		usleep(10000);
 
 		displaybuffer[0] = 0x02;
 		displaybuffer[1] = 0x02;
@@ -140,7 +140,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x02;
 		i2c_smbus_write_i2c_block_data(file, 0x00, sizeof(displaybuffer), (__u8 *)displaybuffer);
 
-		usleep(5000);
+		usleep(10000);
 
 		displaybuffer[0] = 0x04;
 		displaybuffer[1] = 0x04;
@@ -149,7 +149,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x04;
 		i2c_smbus_write_i2c_block_data(file, 0x00, sizeof(displaybuffer), (__u8 *)displaybuffer);
 
-		usleep(5000);
+		usleep(10000);
 
 		displaybuffer[0] = 0x08;
 		displaybuffer[1] = 0x08;
@@ -158,7 +158,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x08;
 		i2c_smbus_write_i2c_block_data(file, 0x00, sizeof(displaybuffer), (__u8 *)displaybuffer);
 
-		usleep(5000);
+		usleep(10000);
 
 		displaybuffer[0] = 0x10;
 		displaybuffer[1] = 0x10;
@@ -167,7 +167,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x10;
 		i2c_smbus_write_i2c_block_data(file, 0x00, sizeof(displaybuffer), (__u8 *)displaybuffer);
 
-		usleep(5000);
+		usleep(10000);
 
 		displaybuffer[0] = 0x20;
 		displaybuffer[1] = 0x20;
@@ -176,7 +176,7 @@ void sendCharacters() {
 		displaybuffer[4] = 0x20;
 		i2c_smbus_write_i2c_block_data(file, 0x00, sizeof(displaybuffer), (__u8 *)displaybuffer);
 
-		usleep(5000);
+		usleep(10000);
 
 	}
 
